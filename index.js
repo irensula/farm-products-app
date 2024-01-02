430e99fed4123e12d31793b7e844569e1aa025ab
irensula <irinawebdev@gmail.com>
2024-01-02 16:40:52 +0200

    new links

--- index.js
+++ index.js
@@ -37,10 +37,32 @@ app.get('/farms/new', (req, res) => {
     res.render('farms/new');
 })
 
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    res.render('farms/show', { farm });
+})
+
 app.post('/farms', async (req, res) => {
     const farm = new Farm(req.body);
     await farm.save();
     res.redirect('/farms');
+})
+
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm })
+})
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`);
 })
 
 // PRODUCT ROUTES
@@ -76,7 +98,7 @@ app.post('/products', wrapAsync(async (req, res, next) => {
 
 app.get('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate('farm', 'name');
     // error - if there is no product
     if (!product) {
         throw new AppError('Product Not Found', 404);
